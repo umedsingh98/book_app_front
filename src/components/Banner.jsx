abfa990af4347d1531bd830bab743f045b7600db
umedsingh98 <umedsingh0898@gmail.com>
2024-09-21 16:54:23 +0530

    responsive changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import banner from "../assets/child.png" 
+import banner from "../assets/child.png"; 
+
 function Banner() {
   return (
     <>
-      <div className="w-full container md:px-20 px-4 left-0 right-0 flex md:flex-row flex-col">
-        <div className=" w-full md:w-1/2 mt-28 md:mt-32">
+      <div className="w-full container md:px-20 px-4 flex md:flex-row flex-col">
+        <div className="w-full md:w-1/2 mt-28 md:mt-32">
           <div className="space-y-8 ml-2">
             <h1 className="md:text-5xl text-4xl font-bold">
               Start learning today with BookPoint.{" "}
@@ -15,16 +16,27 @@ function Banner() {
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500.
             </p>
-            
-          <label className="input input-bordered bg-slate-100 dark:border-slate-400 dark:bg-slate-900 dark:text-white flex items-center gap-2 mt-6">
-            Email:
-            <input type="text" className="grow p-2" placeholder=" Enter your email here..." />
-          </label>
+
+            <div className="flex flex-col space-y-2 mt-6">
+              <label htmlFor="email" className="text-lg">
+                Email:
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="input input-bordered bg-slate-100 dark:border-slate-400 dark:bg-slate-900 dark:text-white p-2"
+                placeholder="Enter your email here..."
+              />
+            </div>
           </div>
-         <button className="btn mt-3 px-6 ml-2 bg-red-700 text-white gap-2 hover:bg-green-600">Signup</button>
+
+          <button className="btn mt-6 px-6 ml-2 bg-red-700 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-600">
+            Signup
+          </button>
         </div>
-        <div className="md:mt-16 md:w-1/3 w-full p-6 md:ml-32">
-         <img src={banner} alt="books"/>
+
+        <div className="md:mt-16 w-full md:w-1/2 p-6 md:ml-32">
+          <img src={banner} alt="Books" className="w-full h-auto" />
         </div>
       </div>
     </>
