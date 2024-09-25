a7aec62021f1e4902afd0c3bf87ea8af03f6fb57
umedsingh98 <umedsingh0898@gmail.com>
2024-09-25 21:04:35 +0530

    loader added

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,60 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import Cards from './Cards';
+import Cards from "./Cards";
 
 function Explore() {
-    
   const [book, setBook] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://bookappbackend-99zi.onrender.com/book/bookportal");
+        const res = await axios.get(
+          "https://bookappbackend-99zi.onrender.com/book/bookportal"
+        );
         console.log(res.data);
         setBook(res.data.filter((data) => data.category === "paid"));
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
     getBook();
   }, []);
   return (
     <>
-    <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
-      <div className="mt-16 py-16 items-center justify-center text-center">
-        <h1 className="text-2xl font-bold md:text-4xl">
-          We're delighted to have you{" "}
-          <span className="text-yellow-500">Here! :</span>
-        </h1>
-        <p className="mt-12 mx-10">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
-          assumenda? Repellendus, iste corrupti? Tempore laudantium
-          repellendus accusamus accusantium sed architecto odio, nisi expedita
-          quas quidem nesciunt debitis dolore non aspernatur praesentium
-          assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
-          animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
-          consequatur!
-        </p>
-        <Link to="/">
-          <button className="mt-8 bg-red-700 text-white px-4 py-2 rounded-md hover:bg-red-500 duration-300">
-            Back
-          </button>
-        </Link>
-      </div>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-16 py-16 items-center justify-center text-center">
+          <h1 className="text-2xl font-bold md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-yellow-500">Here! :</span>
+          </h1>
+          <p className="mt-12 mx-10">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+            assumenda? Repellendus, iste corrupti? Tempore laudantium
+            repellendus accusamus accusantium sed architecto odio, nisi expedita
+            quas quidem nesciunt debitis dolore non aspernatur praesentium
+            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
+            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
+            consequatur!
+          </p>
+          <Link to="/">
+            <button className="mt-8 bg-red-700 text-white px-4 py-2 rounded-md hover:bg-red-500 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold">Explore Your Favourite Books</h1>
+          <hr className="mt-3"></hr>
+        </div>
       <div>
-        <h1 className='text-2xl font-bold'>Explore Your Favourite Books</h1>
-        <hr className='mt-3'></hr>
+        {loading ? (
+          <div className="d-flex justify-content-center align-items-center flex-col gap-2 min-h-[200px]">
+            <div className="w-12 h-12 border-4 border-t-4 border-grey-200 border-t-blue-500 rounded-full animate-spin"></div>
+            <h2 className="text-xl">Loading Data...</h2>
+          </div>
+        ) : (
+          <div className="mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            {book.map((item) => (
+              <Cards items={item} key={item.id} />
+            ))}
+          </div>
+        )}
+        </div>
       </div>
-      <div className='mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-        {
-            book.map((item) => (
-                <Cards items = {item} key={item.id}/>
-            ))
-        }
-      </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
 
-export default Explore
+export default Explore;
--- src/components/FreeBook.jsx
+++ src/components/FreeBook.jsx
@@ -1,7 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
-
 import Cards from "../components/Cards.jsx";
-//for crousel
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -9,25 +7,32 @@ import axios from "axios";
 
 function FreeBook() {
   const [book, setBook] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const sliderRef = useRef(null);
+
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://bookappbackend-99zi.onrender.com/book/bookportal");
+        const res = await axios.get(
+          "https://bookappbackend-99zi.onrender.com/book/bookportal"
+        );
         console.log(res.data);
         setBook(res.data.filter((data) => data.category === "Free"));
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
     getBook();
   }, []);
 
-  let sliderRef = useRef(null);
   const play = () => {
-    sliderRef.slickPlay();
+    if (sliderRef.current) sliderRef.current.slickPlay();
   };
+
   const pause = () => {
-    sliderRef.slickPause();
+    if (sliderRef.current) sliderRef.current.slickPause();
   };
 
   const settings = {
@@ -66,27 +71,35 @@ function FreeBook() {
   };
 
   return (
-    <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 mt-10">
-        <div>
-          <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
-            corporis nulla non suscipit, iure neque earum, consectetur
-            adipisicing elit. Accusantium veritatis alias pariatur ad dolor
-          </p>
-        </div>
-
-        <div>
-          <Slider {...settings}>
-            {book.map((item) => (
-              <Cards items={item} key={item.id} />
-            ))}
-          </Slider>
-        </div>
+    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 mt-10">
+      <div>
+        <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          veritatis alias pariatur ad dolor repudiandae eligendi corporis nulla
+          non suscipit, iure neque earum, consectetur adipisicing elit.
+          Accusantium veritatis alias pariatur ad dolor.
+        </p>
       </div>
-    </>
+
+      <div>
+        {loading ? (
+          <div className="flex flex-col items-center justify-center min-h-[200px] gap-2">
+            {/* Spinner using Tailwind's animate-spin */}
+            <div className="w-12 h-12 border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+            <h2 className="text-xl font-medium text-gray-600">Loading data...</h2>
+          </div>
+        ) : (
+          <Slider {...settings} ref={sliderRef}>
+            {book.length > 0 ? (
+              book.map((item) => <Cards items={item} key={item.id} />)
+            ) : (
+              <p>No free courses available</p>
+            )}
+          </Slider>
+        )}
+      </div>
+    </div>
   );
 }
 
